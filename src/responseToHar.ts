054e1a2eb5ae56c39aa9c792c27c6396f73264bb
Artem Zakharchenko <kettanaito@gmail.com>
2023-09-30 13:16:01 +0200

    fix: rewrite HttpMessage class

--- src/responseToHar.ts
+++ src/responseToHar.ts
@@ -1,6 +1,6 @@
 import * as Har from 'har-format'
 import { toHeaders } from './utils/toHeaders'
-import { toContent } from './utils/toContent'
+import { HttpMessage } from './utils/toHttpMessage'
 
 /**
  * Transforms a given Fetch API `Response` instance to
@@ -10,13 +10,31 @@ export async function responseToHar(response: Response): Promise<Har.Response> {
   const { status, statusText } = response
   const redirectUrl = response.headers.get('Location') || ''
   const headers = toHeaders(response.headers)
-  const content = await toContent(response)
+  const message = await HttpMessage.from(response)
+
+  // Compute the response body size taking any compression
+  // into account. If no explicit "Content-Length" header was set,
+  // inherit the body size from the HTTP message.
+  const contentLength = response.headers.get('Content-Length')
+  const bodySize = contentLength ? Number(contentLength) : message.bodySize
 
   return {
+    httpVersion: HttpMessage.httpVersion,
     status,
     statusText,
     headers,
-    content,
+    /**
+     * @todo Cookies
+     */
+    cookies: [],
+    content: {
+      size: message.bodySize,
+      encoding: message.encoding,
+      mimeType: message.mimeType,
+      text: message.body,
+    },
     redirectURL: redirectUrl,
+    headersSize: message.headersSize,
+    bodySize,
   }
 }
--- src/utils/__test__/toHttpMessage.test.ts
+++ src/utils/__test__/toHttpMessage.test.ts
@@ -1,85 +1,142 @@
-import { httpMessage } from '../toHttpMessage'
+import { HttpMessage } from '../toHttpMessage'
 
-describe(httpMessage.fromRequest, () => {
+describe('from(Request)', () => {
   it('returns http message for a GET request', async () => {
-    expect(
-      await httpMessage.fromRequest(new Request('https://example.com'))
-    ).toBe(`GET https://example.com/ HTTP/1.0\r\n`)
+    const message = await HttpMessage.from(new Request('https://example.com'))
+
+    expect(message.mimeType).toBe('')
+    expect(message.headers).toBe('')
+    expect(message.headersSize).toBe(35)
+    expect(message.body).toBeUndefined()
+    expect(message.bodySize).toBe(0)
+    expect(message.toString()).toBe(`GET https://example.com/ HTTP/1.0\r\n`)
   })
 
   it('returns http message for a POST request', async () => {
-    expect(
-      await httpMessage.fromRequest(
-        new Request('https://example.com', {
-          method: 'POST',
-          body: 'Hello world',
-        })
-      )
-    ).toBe(
+    const message = await HttpMessage.from(
+      new Request('https://example.com', {
+        method: 'POST',
+        body: 'Hello world',
+      })
+    )
+
+    expect(message.mimeType).toBe('text/plain;charset=UTF-8')
+    expect(message.headers).toBe(
+      `content-type: text/plain;charset=UTF-8\r\n\r\n`
+    )
+    expect(message.headersSize).toBe(78)
+    expect(message.body).toBe('Hello world')
+    expect(message.bodySize).toBe(11)
+
+    expect(message.toString()).toBe(
       `POST https://example.com/ HTTP/1.0\r\ncontent-type: text/plain;charset=UTF-8\r\n\r\nHello world`
     )
   })
 
   it('returns http message for a POST request with headers', async () => {
-    expect(
-      await httpMessage.fromRequest(
-        new Request('https://example.com', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Custom-Header': 'Value',
-          },
-          body: JSON.stringify({ id: 1, name: 'John' }),
-        })
-      )
-    ).toBe(
+    const message = await HttpMessage.from(
+      new Request('https://example.com', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Custom-Header': 'Value',
+        },
+        body: JSON.stringify({ id: 1, name: 'John' }),
+      })
+    )
+
+    expect(message.mimeType).toBe('application/json')
+    expect(message.headers).toBe(
+      `content-type: application/json\r\nx-custom-header: Value\r\n\r\n`
+    )
+    expect(message.headersSize).toBe(94)
+    expect(message.body).toBe(`{"id":1,"name":"John"}`)
+    expect(message.bodySize).toBe(22)
+    expect(message.toString()).toBe(
       `POST https://example.com/ HTTP/1.0\r\ncontent-type: application/json\r\nx-custom-header: Value\r\n\r\n{"id":1,"name":"John"}`
     )
   })
 })
 
-describe(httpMessage.fromResponse, () => {
+describe('from(Response)', () => {
   it('returns http message for an empty 200 OK response', async () => {
-    expect(await httpMessage.fromResponse(new Response())).toBe(
-      `HTTP/1.0 200 OK\r\n`
-    )
+    const message = await HttpMessage.from(new Response())
+
+    expect(message.mimeType).toBe('')
+    expect(message.headers).toBe('')
+    expect(message.headersSize).toBe(17)
+    expect(message.body).toBeUndefined()
+    expect(message.bodySize).toBe(0)
+    expect(message.toString()).toBe(`HTTP/1.0 200 OK\r\n`)
   })
 
   it('returns http message for response with headers', async () => {
-    const headers = new Headers([
-      ['Content-Type', 'application/json'],
-      ['X-Custom-Header', 'Value'],
-    ])
-    expect(
-      await httpMessage.fromResponse(
-        new Response(null, { status: 201, headers })
-      )
-    ).toBe(
+    const message = await HttpMessage.from(
+      new Response(null, {
+        status: 201,
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Custom-Header': 'Value',
+        },
+      })
+    )
+
+    expect(message.mimeType).toBe('application/json')
+    expect(message.headers).toBe(
+      `content-type: application/json\r\nx-custom-header: Value\r\n`
+    )
+    expect(message.headersSize).toBe(78)
+    expect(message.body).toBeUndefined()
+    expect(message.bodySize).toBe(0)
+    expect(message.toString()).toBe(
       `HTTP/1.0 201 Created\r\ncontent-type: application/json\r\nx-custom-header: Value\r\n\r\n`
     )
   })
 
   it('returns http message for response with text body', async () => {
-    expect(
-      await httpMessage.fromResponse(
-        new Response('Hello world', {
-          headers: {
-            'Content-Type': 'text/plain;charset=UTF-8',
-          },
-        })
-      )
-    ).toBe(
+    const message = await HttpMessage.from(
+      new Response('Hello world', {
+        headers: {
+          'Content-Type': 'text/plain;charset=UTF-8',
+        },
+      })
+    )
+
+    expect(message.mimeType).toBe('text/plain;charset=UTF-8')
+    expect(message.headers).toBe(
+      `content-type: text/plain;charset=UTF-8\r\n\r\n`
+    )
+    expect(message.headersSize).toBe(59)
+    expect(message.body).toBe('Hello world')
+    expect(message.bodySize).toBe(11)
+    expect(message.toString()).toBe(
       `HTTP/1.0 200 OK\r\ncontent-type: text/plain;charset=UTF-8\r\n\r\nHello world`
     )
   })
 
   it('returns http message for response with multi-line body', async () => {
-    expect(
-      await httpMessage.fromResponse(
-        new Response('Hello\r\nfrom\r\n\r\nserver!')
-      )
-    ).toBe(
+    const message = await HttpMessage.from(
+      new Response('Hello\r\nfrom\r\n\r\nserver!')
+    )
+
+    expect(message.mimeType).toBe('text/plain;charset=UTF-8')
+    expect(message.headers).toBe(
+      `content-type: text/plain;charset=UTF-8\r\n\r\n`
+    )
+    expect(message.headersSize).toBe(59)
+    expect(message.body).toBe('Hello\r\nfrom\r\n\r\nserver!')
+    expect(message.bodySize).toBe(22)
+    expect(message.toString()).toBe(
       `HTTP/1.0 200 OK\r\ncontent-type: text/plain;charset=UTF-8\r\n\r\nHello\r\nfrom\r\n\r\nserver!`
     )
+  })
+
+  it('sets "bodySize" to 0 for cached response (304)', async () => {
+    const message = await HttpMessage.from(new Response(null, { status: 304 }))
+
+    /**
+     * @see http://www.softwareishard.com/blog/har-12-spec/#response
+     */
+    expect(message.bodySize).toBe(0)
   })
 })
